0667397e3ac33263b3a542e780734428ad450bd9
shantanujumale <shantanujumale92@gmail.com>
2025-05-06 14:51:54 +0530

    Create 3.1.1.c

---
+++ 3.1.1.c
@@ -0,0 +1,36 @@
+void display(int arr[15],int size){
+for(int i=0;i<size;i++){
+	printf("%d ",arr[i]);
+}
+	printf("\n");
+}
+void merge(int arr[],int left,int mid,int right){
+	int i,j,k;
+	int n1=mid-left+1;
+	int n2=right-mid;
+	int L[n1],R[n2];
+	
+	for(int i=0;i<n1;i++){
+		L[i]=arr[left+i];
+	}
+	for(int j=0;j<n2;j++){
+		R[j]=arr[mid+1+j];
+	}
+	i=0;
+	j=0;
+	k=left;
+	while(i<n1&&j<n2){
+		if(L[i]<=R[j]){
+			arr[k]=L[i];
+			i++;
+		}
+		else{
+			arr[k]=R[j];
+			j++;
+		}
+		k++;
+	}
+	
+
+	while(i<n1){
+		arr[k]=L[i];
